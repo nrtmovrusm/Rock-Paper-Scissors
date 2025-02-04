62cd043dadaed81210777759e1e1657005d952d2
nrtmovrusm <192477100+nrtmovrusm@users.noreply.github.com>
2025-02-04 09:53:58 -0800

    Added UI including buttons for human selection for rps

--- javascript.js
+++ javascript.js
@@ -12,47 +12,66 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = (prompt("Rock, paper, or scissors?")).toLowerCase();
-    if (humanChoice === "rock") {
-        return "rock";
-    } else if (humanChoice === "paper") {
-        return "paper";
-    } else if (humanChoice === "scissors") {
-        return "scissors";
-    } else {
-        humanChoice = getHumanChoice();
-        return humanChoice;
-    }
-}
+const para = document.createElement("p");
+document.body.appendChild(para);
+
+const gameResults = document.createElement("div");
+document.body.appendChild(gameResults);
+
+const score = document.createElement("div");
+document.body.appendChild(score);
+const space = document.createElement("p");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let humanChoice = button.id;
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    });
+})
 
 function playRound(humanChoice, computerChoice) {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
+    const roundResults = document.createElement("div");
+
     if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
         humanScore++;
-        console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1,)} beats ${computerChoice}.`);
-        return `You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1,)} beats ${computerChoice}.`;
+        roundResults.textContent = `You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1,)} beats ${computerChoice}.`;
     } else if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
         computerScore++;
-        console.log(`You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1,)} beats ${humanChoice}.`);
-        return `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1,)} beats ${humanChoice}.`;
+        roundResults.textContent = `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1,)} beats ${humanChoice}.`;
     } else if (humanChoice === computerChoice) {
-        console.log(`Tie!`);
-        return `Tie!`;
+        roundResults.textContent = `Tie!`;
+    }
+
+    score.textContent = `SCORE: Human: ${humanScore}, Computer: ${computerScore}`;
+
+    gameResults.appendChild(roundResults);
+    gameResults.appendChild(space);
+    gameResults.appendChild(score);
+
+    if (humanScore >= 5 || computerScore >= 5) {
+        endGame();
     }
 }
 
-function playGame() {
-    for (let i = 1; i <= 5; i++) {
-        playRound();
-    }
+function endGame() {
+    const paragraphBreak = document.createElement("p");
+    const endResults = document.createElement("div");
 
     if (humanScore > computerScore) {
-        return `Congratulations! You have won! Human score: ${humanScore}. Computer score ${computerScore}.`;
+        endResults.textContent = `Congratulations! You have won!`;
     } else if (computerScore > humanScore) {
-        return `Uh oh! You have lost! Human score: ${humanScore}. Computer score ${computerScore}.`;
-    } else if (computerScore = humanScore) {
-        return `Tie game! Human score: ${humanScore}. Computer score ${computerScore}.`
+        endResults.textContent = `Uh oh! You have lost!`;
+    } else if (computerScore == humanScore) {
+        endResults.textContent = `Tie game!`;
     }
+
+    gameResults.appendChild(paragraphBreak);
+    gameResults.appendChild(endResults);
+
+    buttons.forEach((button) => {
+        button.disabled = true;
+    })
 }
